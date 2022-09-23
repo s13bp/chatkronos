821b8818bb13732d7447d9790c86d9e1e27ac175
s13bp <86206902+s13bp@users.noreply.github.com>
2022-09-23 22:01:36 +0300

    Add files via upload

---
+++ precache-manifest.1ec16bac5e05f8308dbeef92f5fed9e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db629a92cd32104ecf1eae07c6f2bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fed7961672eb4dfd9f79",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "f4008facadf0debbb732",
+    "url": "/static/js/2.61ae9a6c.chunk.js"
+  },
+  {
+    "revision": "fed7961672eb4dfd9f79",
+    "url": "/static/js/main.7f0a8589.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
